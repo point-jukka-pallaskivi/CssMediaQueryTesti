7dda551a5af4318adf57544456ac747747801c28
Jukka Pallaskivi <jukka.pallaskivi@stud.pointcollege.fi>
2016-04-05 10:30:25 +0300

    Ensimmäinen testi

---
+++ CssMediaQueryTesti/CssMediaQueryTesti/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CssMediaQueryTesti.Startup))]
+namespace CssMediaQueryTesti
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
